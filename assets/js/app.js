3d4ff866f20c550c89467cde41b2203b5dced630
Rougui224 <rouguiatoubarry559@gmail.com>
2024-09-03 16:46:17 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,156 @@
+import { quizSessions } from "./data.js";
+// Générer un nombre aléatoire entre 0 et 4
+let randomVariable,
+  lastRandomVariable = "",
+  index = 0,
+  score = 0,
+  timeoutId,
+  currentSection,
+  UserCorrectAnswers = [],
+  wrongAnswers = [],
+  prevRandomVariables =
+    JSON.parse(localStorage.getItem("prevRandomVariables")) || [];
+function updatePRVtoLogalStorage() {
+  localStorage.setItem(
+    "prevRandomVariables",
+    JSON.stringify(prevRandomVariables)
+  );
+} // Affiche un nombre aléatoire entre 0 et 4
+
+function CheckRamdomVarable() {
+  if (prevRandomVariables.length === quizSessions.length) {
+    lastRandomVariable = prevRandomVariables[prevRandomVariables.length - 1];
+    prevRandomVariables = [];
+    updatePRVtoLogalStorage();
+  }
+  do {
+    randomVariable = Math.floor(Math.random() * quizSessions.length);
+  } while (
+    prevRandomVariables.includes(randomVariable) ||
+    randomVariable === lastRandomVariable
+  );
+  prevRandomVariables.push(randomVariable);
+  updatePRVtoLogalStorage();
+  lastRandomVariable = "";
+}
+function calculateScore(correctAnswers) {
+  if (
+    UserCorrectAnswers.length === correctAnswers.length &&
+    wrongAnswers.length === 0
+  ) {
+    score = score + 1;
+  }
+}
+function showCheck() {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(function () {
+    checkQuiz();
+  }, 10000);
+}
+function startQuiz() {
+  currentSection = quizSessions[randomVariable].questions;
+
+  if (index === currentSection.length) {
+    $(".app_quiz").removeClass("starting").addClass("exiting");
+    $(".modal").show();
+    $(".overlay").show();
+    $(".progress").hide();
+    if (score >= 5) {
+      $(".modal p").text(
+        `Congratulations! You scored ${score}/${currentSection.length}! Keep it up!`
+      );
+    } else {
+      $(".modal p").text(
+        `Good try! Your score is ${score}/${currentSection.length}. You can do even better next time!`
+      );
+    }
+
+    $(".modal button").text("Continue");
+    $(".app_questionNumber").text("");
+
+    score = 0;
+    index = 0;
+    clearTimeout(timeoutId);
+    return;
+  }
+  const areaQuestionElement = $(".app_quiz_question");
+  const optionsListElements = $(".app_quiz_options p");
+  optionsListElements
+    .removeClass("active")
+    .removeClass("error")
+    .removeClass("done");
+  $(".progress").show();
+  $(".validateButton").show();
+  $(".nextButton").hide();
+  $(".app_questionNumber").text(`${index + 1}`);
+  const question = currentSection[index].question,
+    options = currentSection[index].options,
+    correctAnswers = currentSection[index].correctAnswers;
+  areaQuestionElement.text(question);
+  optionsListElements.each(function (index, element) {
+    $(element).text(options[index]);
+    $(element)
+      .off("click")
+      .on("click", function () {
+        $(this).addClass("active");
+        if (correctAnswers.includes(index)) {
+          UserCorrectAnswers.push(index);
+        } else {
+          wrongAnswers.push(index);
+        }
+      });
+  });
+}
+function startTimingElement() {
+  const progressElement = $(".progress");
+
+  // Supprimer la classe "timing"
+  progressElement.removeClass("timing");
+  progressElement.show();
+  // Forcer un reflow : Accéder à une propriété telle que offsetWidth ou offsetHeight force le navigateur à recalculer le style et re-rendre l'élément, ce qui permet de "réinitialiser" l'animation.
+  void progressElement[0].offsetWidth;
+
+  // Réappliquer la classe "timing" pour redémarrer l'animation
+  progressElement.addClass("timing");
+}
+function checkQuiz() {
+  $(".progress").hide();
+  $(".validateButton").hide();
+  $(".nextButton").show();
+  const correctAnswers = currentSection[index].correctAnswers;
+
+  $(".app_quiz_options p").each(function (index, element) {
+    if (correctAnswers.includes(index)) {
+      $(element).addClass("done");
+    } else {
+      if (wrongAnswers.length > 0) {
+        $(element).addClass("error");
+      }
+    }
+  });
+  calculateScore(correctAnswers);
+  clearTimeout(timeoutId);
+  UserCorrectAnswers = [];
+  wrongAnswers = [];
+  index++;
+}
+$(".validateButton").click(function () {
+  checkQuiz();
+});
+$(".nextButton").click(function () {
+  startQuiz();
+  startTimingElement();
+  showCheck();
+});
+$(".modal").click(function () {
+  CheckRamdomVarable();
+  startQuiz();
+  startTimingElement();
+  showCheck();
+
+  $(this).hide();
+  $(".overlay").hide();
+  $(".app_quiz").removeClass("exiting").addClass("starting");
+});
---
+++ assets/js/data.js
@@ -0,0 +1,317 @@
+export const quizSessions = [
+  {
+    session: 1,
+    questions: [
+      {
+        question: "Which of the following are programming languages?",
+        options: ["Python", "HTML", "Java", "SQL"],
+        correctAnswers: [0, 2, 3], // HTML est incorrecte
+      },
+      {
+        question: "What are components of a computer's CPU?",
+        options: [
+          "ALU (Arithmetic Logic Unit)",
+          "Cache Memory",
+          "Power Supply",
+          "Control Unit",
+        ],
+        correctAnswers: [0, 1, 3], // Power Supply est incorrecte
+      },
+      {
+        question: "Which of the following are JavaScript frameworks?",
+        options: ["Angular", "Laravel", "React", "Django"],
+        correctAnswers: [0, 2], // Laravel et Django sont incorrects
+      },
+      {
+        question: "Which of the following are types of databases?",
+        options: ["MySQL", "MongoDB", "PostgreSQL", "Apache"],
+        correctAnswers: [0, 1, 2], // Apache est incorrecte
+      },
+      {
+        question: "What are valid file extensions for an image?",
+        options: [".jpg", ".png", ".txt", ".gif"],
+        correctAnswers: [0, 1, 3], // .txt est incorrecte
+      },
+      {
+        question: "Which of these are cloud service providers?",
+        options: ["AWS", "Google Cloud", "Azure", "Apache Spark"],
+        correctAnswers: [0, 1, 2], // Apache Spark est incorrecte
+      },
+      {
+        question: "Which of the following are version control systems?",
+        options: ["Git", "SVN", "Docker", "Mercurial"],
+        correctAnswers: [0, 1, 3], // Docker est incorrecte
+      },
+      {
+        question: "Which of these are front-end development technologies?",
+        options: ["CSS", "HTML", "Node.js", "JavaScript"],
+        correctAnswers: [0, 1, 3], // Node.js est incorrecte
+      },
+      {
+        question: "What are common HTTP methods?",
+        options: ["GET", "PUT", "DELETE", "CONNECT"],
+        correctAnswers: [0, 1, 2], // CONNECT est incorrecte
+      },
+      {
+        question: "Which of the following are types of cyber attacks?",
+        options: ["Phishing", "DDoS", "SQL Injection", "Binary Attack"],
+        correctAnswers: [0, 1, 2], // Binary Attack est incorrecte
+      },
+    ],
+  },
+  {
+    session: 2,
+    questions: [
+      {
+        question: "Which of these are types of machine learning?",
+        options: [
+          "Supervised Learning",
+          "Reinforcement Learning",
+          "Neural Networks",
+          "Unsupervised Learning",
+        ],
+        correctAnswers: [0, 1, 3], // Neural Networks est incorrecte
+      },
+      {
+        question: "Which programming languages are used for data analysis?",
+        options: ["Python", "R", "JavaScript", "SQL"],
+        correctAnswers: [0, 1, 3], // JavaScript est incorrecte
+      },
+      {
+        question: "Which of these are types of operating systems?",
+        options: ["Linux", "Android", "iOS", "Hadoop"],
+        correctAnswers: [0, 1, 2], // Hadoop est incorrecte
+      },
+      {
+        question: "What are common NoSQL databases?",
+        options: ["Cassandra", "CouchDB", "Oracle", "DynamoDB"],
+        correctAnswers: [0, 1, 3], // Oracle est incorrecte
+      },
+      {
+        question: "Which of the following are cloud computing models?",
+        options: ["SaaS", "PaaS", "IaaS", "DaaS"],
+        correctAnswers: [0, 1, 2], // DaaS est incorrecte
+      },
+      {
+        question: "Which of these languages are used for backend development?",
+        options: ["Python", "Ruby", "HTML", "Java"],
+        correctAnswers: [0, 1, 3], // HTML est incorrecte
+      },
+      {
+        question: "Which of the following are continuous integration tools?",
+        options: ["Jenkins", "Travis CI", "GitHub Actions", "Maven"],
+        correctAnswers: [0, 1, 2], // Maven est incorrecte
+      },
+      {
+        question: "What are common data visualization libraries in Python?",
+        options: ["Matplotlib", "Pandas", "Seaborn", "D3.js"],
+        correctAnswers: [0, 2, 3], // Pandas est incorrecte
+      },
+      {
+        question: "Which of these are relational database management systems?",
+        options: ["Oracle", "PostgreSQL", "MariaDB", "Redis"],
+        correctAnswers: [0, 1, 2], // Redis est incorrecte
+      },
+      {
+        question:
+          "Which of these are principles of Object-Oriented Programming?",
+        options: [
+          "Encapsulation",
+          "Polymorphism",
+          "Abstraction",
+          "Refactoring",
+        ],
+        correctAnswers: [0, 1, 2], // Refactoring est incorrecte
+      },
+    ],
+  },
+  {
+    session: 3,
+    questions: [
+      {
+        question: "Which of these are common Java IDEs?",
+        options: ["IntelliJ IDEA", "Eclipse", "PyCharm", "NetBeans"],
+        correctAnswers: [0, 1, 3], // PyCharm est incorrecte
+      },
+      {
+        question: "Which of the following are common network protocols?",
+        options: ["HTTP", "FTP", "SMTP", "HTML"],
+        correctAnswers: [0, 1, 2], // HTML est incorrecte
+      },
+      {
+        question: "Which of these are considered cloud storage services?",
+        options: ["Google Drive", "Dropbox", "Amazon S3", "Microsoft OneDrive"],
+        correctAnswers: [0, 1, 2], // Microsoft OneDrive est incorrecte
+      },
+      {
+        question: "What are examples of markup languages?",
+        options: ["HTML", "XML", "JSON", "Markdown"],
+        correctAnswers: [0, 1, 3], // JSON est incorrecte
+      },
+      {
+        question:
+          "Which of the following are considered software development methodologies?",
+        options: ["Agile", "Waterfall", "Scrum", "MVC"],
+        correctAnswers: [0, 1, 2], // MVC est incorrecte
+      },
+      {
+        question: "Which of these are types of data structures?",
+        options: ["Array", "Queue", "Stack", "File System"],
+        correctAnswers: [0, 1, 2], // File System est incorrecte
+      },
+      {
+        question: "What are popular open-source software licenses?",
+        options: [
+          "MIT License",
+          "Apache License 2.0",
+          "GPL (General Public License)",
+          "Microsoft EULA",
+        ],
+        correctAnswers: [0, 1, 2], // Microsoft EULA est incorrecte
+      },
+      {
+        question: "Which of these are virtualization tools?",
+        options: ["VMware", "VirtualBox", "Hyper-V", "Docker"],
+        correctAnswers: [0, 1, 2], // Docker est incorrecte
+      },
+      {
+        question: "What are common tools for continuous deployment?",
+        options: ["Docker", "Kubernetes", "Ansible", "Jenkins"],
+        correctAnswers: [0, 1, 3], // Ansible est incorrecte
+      },
+      {
+        question: "Which of these are network topologies?",
+        options: ["Star", "Ring", "Mesh", "Eclipse"],
+        correctAnswers: [0, 1, 2], // Eclipse est incorrecte
+      },
+    ],
+  },
+  {
+    session: 4,
+    questions: [
+      {
+        question: "Which of these are relational databases?",
+        options: ["MySQL", "PostgreSQL", "Oracle", "MongoDB"],
+        correctAnswers: [0, 1, 2], // MongoDB est incorrecte
+      },
+      {
+        question: "What are components of a web application?",
+        options: ["Frontend", "Backend", "Database", "Operating System"],
+        correctAnswers: [0, 1, 2], // Operating System est incorrecte
+      },
+      {
+        question: "Which of the following are valid data types in JavaScript?",
+        options: ["String", "Number", "Boolean", "Character"],
+        correctAnswers: [0, 1, 2], // Character est incorrecte
+      },
+      {
+        question: "What are common operating systems for mobile devices?",
+        options: ["iOS", "Android", "Windows Phone", "Linux"],
+        correctAnswers: [0, 1, 2], // Linux est incorrecte
+      },
+      {
+        question: "Which of these are CSS frameworks?",
+        options: ["Bootstrap", "Foundation", "Tailwind", "jQuery"],
+        correctAnswers: [0, 1, 2], // jQuery est incorrecte
+      },
+      {
+        question: "Which of the following are programming paradigms?",
+        options: ["Object-Oriented", "Functional", "Procedural", "Declarative"],
+        correctAnswers: [0, 1, 2], // Déclarative est incorrecte
+      },
+      {
+        question: "Which of these are data serialization formats?",
+        options: ["JSON", "XML", "YAML", "CSV"],
+        correctAnswers: [0, 1, 2], // CSV est incorrecte
+      },
+      {
+        question: "What are common front-end web development languages?",
+        options: ["HTML", "CSS", "JavaScript", "Python"],
+        correctAnswers: [0, 1, 2], // Python est incorrecte
+      },
+      {
+        question:
+          "Which of these are examples of server-side scripting languages?",
+        options: ["PHP", "Node.js", "Python", "JavaScript"],
+        correctAnswers: [0, 1, 2], // JavaScript est incorrecte
+      },
+      {
+        question: "What are common RESTful API methods?",
+        options: ["GET", "POST", "PUT", "DROP"],
+        correctAnswers: [0, 1, 2], // DROP est incorrecte
+      },
+    ],
+  },
+  {
+    session: 5,
+    questions: [
+      {
+        question: "Which of the following are JavaScript libraries?",
+        options: ["React", "Vue.js", "Angular", "jQuery"],
+        correctAnswers: [0, 1, 3], // Angular est incorrecte
+      },
+      {
+        question: "What are common version control commands in Git?",
+        options: ["git commit", "git push", "git merge", "git branch"],
+        correctAnswers: [0, 1, 2], // git branch est incorrecte
+      },
+      {
+        question: "Which of these are Python web frameworks?",
+        options: ["Django", "Flask", "Pyramid", "Rails"],
+        correctAnswers: [0, 1, 2], // Rails est incorrecte
+      },
+      {
+        question: "Which of the following are basic networking devices?",
+        options: ["Router", "Switch", "Hub", "Bridge"],
+        correctAnswers: [0, 1, 2], // Bridge est incorrecte
+      },
+      {
+        question: "What are common data analysis tools?",
+        options: ["Pandas", "NumPy", "Excel", "Hadoop"],
+        correctAnswers: [0, 1, 2], // Hadoop est incorrecte
+      },
+      {
+        question:
+          "Which of the following are methods of securing web applications?",
+        options: [
+          "HTTPS",
+          "CSP (Content Security Policy)",
+          "WAF (Web Application Firewall)",
+          "VPN",
+        ],
+        correctAnswers: [0, 1, 2], // VPN est incorrecte
+      },
+      {
+        question: "What are examples of machine learning algorithms?",
+        options: ["Linear Regression", "K-Means", "Random Forest", "SVM"],
+        correctAnswers: [0, 1, 2], // SVM est incorrecte
+      },
+      {
+        question: "Which of these are principles of Responsive Web Design?",
+        options: [
+          "Fluid Grids",
+          "Flexible Images",
+          "Media Queries",
+          "CSS Resets",
+        ],
+        correctAnswers: [0, 1, 2], // CSS Resets est incorrecte
+      },
+      {
+        question: "What are examples of code quality tools?",
+        options: ["ESLint", "Prettier", "JSHint", "Stylelint"],
+        correctAnswers: [0, 1, 2], // Stylelint est incorrecte
+      },
+      {
+        question:
+          "Which of the following are types of testing in software development?",
+        options: [
+          "Unit Testing",
+          "Integration Testing",
+          "System Testing",
+          "Regression Testing",
+        ],
+        correctAnswers: [0, 1, 2], // Regression Testing est incorrecte
+      },
+    ],
+  },
+];
